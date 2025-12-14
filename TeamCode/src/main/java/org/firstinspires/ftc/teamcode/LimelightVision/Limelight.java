b0b457da161e38e7ba685a2b2a8db006c55b138f
orwell <orwellg19847@gmail.com>
2025-12-15 02:55:09 +0300

    Update Limelight.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightVision/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightVision/Limelight.java
@@ -1,2 +1,19 @@
-limelight=hardwaremap.get(LimelightConstants.class,"limelight");
-limelight.start;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "LimelightTest")
+public class ApriltagLimelightTest extends OpMode {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+    }
+}
